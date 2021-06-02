d8fcc96ac059b318aebee3ca24b543c8418e457a
pemudakoding <pemudakoding@gmail.com>
2021-06-02 22:37:02 +0800

    added:  categories component in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
 
 import landingPage from "json/landingPage.json";
 
@@ -24,6 +25,7 @@ export default class LandingPage extends Component {
                     refMostPicked={this.refMostPicked}
                     data={landingPage.mostPicked}
                 />
+                <Categories data={landingPage.categories} />
             </React.Fragment>
         );
     }
